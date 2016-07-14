aa58c9a6870e85832d695e46f310ae5eea559f8f
小奔奔 <bunwong@qq.com>
2016-07-14 15:06:20 +0800

    restore session.h

---
+++ azalea/session.h
@@ -0,0 +1,19 @@
+/*
+ * azalea/session.h
+ *
+ * Created by Bun Wong on 16-6-28.
+ */
+
+#ifndef AZALEA_SESSION_H_
+#define AZALEA_SESSION_H_
+
+AZALEA_STARTUP_FUNCTION(session);
+
+PHP_METHOD(azalea_session, __construct) {};
+PHP_METHOD(azalea_session, get);
+PHP_METHOD(azalea_session, set);
+PHP_METHOD(azalea_session, clean);
+
+extern zend_class_entry *azalea_session_ce;
+
+#endif /* AZALEA_SESSION_H_ */
