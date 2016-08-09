ad8a4003f2ee45e58567a28ae4efb8c0d8e3bc18
小奔奔 <bunwong@qq.com>
2016-08-09 22:43:22 +0800

    move template function to global function

--- azalea/view.c
+++ azalea/view.c
@@ -188,16 +188,16 @@ PHP_METHOD(azalea_view, render)
 	}
 	// start render
 	php_output_start_user(NULL, 0, PHP_OUTPUT_HANDLER_STDFLAGS);
-	azaleaRegisterTemplateFunctions();
+//	azaleaRegisterTemplateFunctions();
 	if (!azaleaRequire(ZSTR_VAL(tplPath), ZSTR_LEN(tplPath))) {
-		azaleaUnregisterTemplateFunctions();
+//		azaleaUnregisterTemplateFunctions();
 		zend_string_release(tplPath);
 		zend_string *message = strpprintf(0, "Failed to open template file `%s.phtml`.", ZSTR_VAL(tplname));
 		throw404(message);
 		zend_string_release(message);
 		RETURN_FALSE;
 	}
-	azaleaUnregisterTemplateFunctions();
+//	azaleaUnregisterTemplateFunctions();
 	zend_string_release(tplPath);
 	php_output_get_contents(return_value);
 	php_output_discard();
